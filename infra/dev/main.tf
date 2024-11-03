0f6d19f98aa0a2ca82b158d0aa287bbb2d5600f0
Law78 <l.franceschini@gmail.com>
2024-11-03 09:51:52 +0100

    Add Resource Group

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -36,52 +36,52 @@ data "azurerm_subscription" "current" {
 }
 
 resource "azurerm_role_assignment" "this" {
-  for_each = toset(var.azure_roles)
-  scope    = data.azurerm_subscription.current.id
-  principal_id   = azuread_service_principal.this.object_id
-  principal_type = "ServicePrincipal"
-  role_definition_name = each.value
+  for_each                         = toset(var.azure_roles)
+  scope                            = data.azurerm_subscription.current.id
+  principal_id                     = azuread_service_principal.this.object_id
+  principal_type                   = "ServicePrincipal"
+  role_definition_name             = each.value
   skip_service_principal_aad_check = true
 }
 
 resource "azuread_application_federated_identity_credential" "branches" {
-  for_each = toset(var.branches)
-  application_id = azuread_application.this.object_id
-  display_name = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
-  description = "Credenziali di identità federata GitHub"
-  subject = "repo:${var.github_organization_name}/${var.github_repository_name}:ref:refs/heads/${each.value}"
-  audiences = ["api://AzureADTokenExchange"]
-  issuer = "https://token.actions.githubusercontent.com"
+  for_each       = toset(var.branches)
+  application_id = "/applications/${azuread_application.this.object_id}"
+  display_name   = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
+  description    = "GitHub federated identity credentials"
+  subject        = "repo:${var.github_organization_name}/${var.github_repository_name}:ref:refs/heads/${each.value}"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
 }
 
 resource "azuread_application_federated_identity_credential" "tags" {
-  for_each = toset(var.tags)
-  application_id = azuread_application.this.object_id
-  display_name = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
-  description = "Credenziali di identità federata GitHub"
-  subject = "repo:${var.github_organization_name}/${var.github_repository_name}:ref:refs/tags/${each.value}"
-  audiences = ["api://AzureADTokenExchange"]
-  issuer = "https://token.actions.githubusercontent.com"
+  for_each       = toset(var.tags)
+  application_id = "/applications/${azuread_application.this.object_id}"
+  display_name   = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
+  description    = "GitHub federated identity credentials"
+  subject        = "repo:${var.github_organization_name}/${var.github_repository_name}:ref:refs/tags/${each.value}"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
 }
 
 resource "azuread_application_federated_identity_credential" "environments" {
-  for_each = toset(var.environments)
-  application_id = azuread_application.this.object_id
-  display_name = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
-  description = "Credenziali di identità federata GitHub"
-  subject = "repo:${var.github_organization_name}/${var.github_repository_name}:environment:${each.value}"
-  audiences = ["api://AzureADTokenExchange"]
-  issuer = "https://token.actions.githubusercontent.com"
+  for_each       = toset(var.environments)
+  application_id = "/applications/${azuread_application.this.object_id}"
+  display_name   = "github-${var.github_organization_name}.${var.github_repository_name}-${each.value}"
+  description    = "GitHub federated identity credentials"
+  subject        = "repo:${var.github_organization_name}/${var.github_repository_name}:environment:${each.value}"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
 }
 
 resource "azuread_application_federated_identity_credential" "pull_request" {
-  count = var.pull_request ? 1 : 0
-  application_id = azuread_application.this.object_id
-  display_name = "github-${var.github_organization_name}.${var.github_repository_name}-pr"
-  description = "Credenziali di identità federata GitHub"
-  subject = "repo:${var.github_organization_name}/${var.github_repository_name}:pull_request"
-  audiences = ["api://AzureADTokenExchange"]
-  issuer = "https://token.actions.githubusercontent.com"
+  count          = var.pull_request ? 1 : 0
+  application_id = "/applications/${azuread_application.this.object_id}"
+  display_name   = "github-${var.github_organization_name}.${var.github_repository_name}-pr"
+  description    = "GitHub federated identity credentials"
+  subject        = "repo:${var.github_organization_name}/${var.github_repository_name}:pull_request"
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
 }
 
 data "github_repository" "this" {
@@ -89,19 +89,19 @@ data "github_repository" "this" {
 }
 
 resource "github_actions_secret" "azure_client_id" {
-  repository = data.github_repository.this.name
-  secret_name = "AZURE_CLIENT_ID"
+  repository      = data.github_repository.this.name
+  secret_name     = "AZURE_CLIENT_ID"
   plaintext_value = azuread_application.this.client_id
 }
 
 resource "github_actions_secret" "azure_tenant_id" {
-  repository = data.github_repository.this.name
-  secret_name = "AZURE_TENANT_ID"
+  repository      = data.github_repository.this.name
+  secret_name     = "AZURE_TENANT_ID"
   plaintext_value = data.azurerm_subscription.current.tenant_id
 }
 
 resource "github_actions_secret" "azure_subscription_id" {
-  repository = data.github_repository.this.name
-  secret_name = "AZURE_SUBSCRIPTION_ID"
+  repository      = data.github_repository.this.name
+  secret_name     = "AZURE_SUBSCRIPTION_ID"
   plaintext_value = data.azurerm_subscription.current.subscription_id
 }
